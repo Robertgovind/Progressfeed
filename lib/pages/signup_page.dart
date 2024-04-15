2887b94275ebad17994ce29f29298d4d63918a7c
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-04-15 17:22:53 +0545

    linking pages

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -12,7 +12,111 @@ class _SignupPageState extends State<SignupPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(),
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Column(
+                children: [
+                  Text(
+                    "Signup",
+                    style: TextStyle(
+                        fontSize: 28,
+                        letterSpacing: 2,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  Text(
+                    "as",
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Column(
+                  children: [
+                    Container(
+                      height: 120,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.amberAccent,
+                        borderRadius: BorderRadius.circular(60),
+                      ),
+                    ),
+                    const Text(
+                      "Supervisor",
+                      style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Column(
+                  children: [
+                    Container(
+                      height: 120,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.amberAccent,
+                        borderRadius: BorderRadius.circular(60),
+                      ),
+                    ),
+                    const Text(
+                      "Contractor",
+                      style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, "/user/signupPage");
+                },
+                child: Column(
+                  children: [
+                    Container(
+                      height: 120,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.amberAccent,
+                        borderRadius: BorderRadius.circular(60),
+                      ),
+                    ),
+                    const Text(
+                      "User",
+                      style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () => Navigator.pushNamed(context, "/loginPage"),
+                child: const Text(
+                  "Alredy have an account",
+                  style: TextStyle(fontSize: 18, color: Colors.blue),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
