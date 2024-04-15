2db25ee00ac98ac7fa29ea0f03e81f2e3ea5c78b
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-04-15 17:25:04 +0545

    linking pages and working on layout

--- lib/pages/user/signup_page.dart
+++ lib/pages/user/signup_page.dart
@@ -1,18 +1,146 @@
 import 'package:flutter/material.dart';
 
-class SignupPage extends StatefulWidget {
-  const SignupPage({super.key});
+class UserSignupPage extends StatefulWidget {
+  const UserSignupPage({super.key});
 
   @override
-  State<SignupPage> createState() => _SignupPageState();
+  State<UserSignupPage> createState() => _UserSignupPageState();
 }
 
-class _SignupPageState extends State<SignupPage> {
+class _UserSignupPageState extends State<UserSignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text("Create Account")),
       body: SafeArea(
-        child: Container(),
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 18, vertical: 18),
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Full Name",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 8),
+                      border: InputBorder.none,
+                      disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Email",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 8),
+                      fillColor: Colors.white,
+                      border: InputBorder.none,
+                      disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Phone No",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 8),
+                      fillColor: Colors.white,
+                      border: InputBorder.none,
+                      disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Address",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 8),
+                      fillColor: Colors.white,
+                      border: InputBorder.none,
+                      disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 35,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: MaterialButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/user/setPassword");
+                  },
+                  child: const Text(
+                    "Next",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
