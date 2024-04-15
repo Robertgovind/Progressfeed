074178a618af4857f2b6084c1aae36ffc654f397
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-04-15 17:19:05 +0545

    setup for work

---
+++ lib/pages/add_post_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddPostPage extends StatefulWidget {
+  const AddPostPage({super.key});
+
+  @override
+  State<AddPostPage> createState() => _AddPostPageState();
+}
+
+class _AddPostPageState extends State<AddPostPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Add post"),
+    );
+  }
+}
